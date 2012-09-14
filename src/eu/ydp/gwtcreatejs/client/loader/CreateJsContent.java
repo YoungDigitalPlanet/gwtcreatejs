f34aa5c5bdce87e5e8bae420bb41c724b79cd13b
mkaldonek <mkaldonek@ydp.com.pl>
2012-09-14 06:29:58 +0000

    removed outline from canvas
    git-svn-id: https://svn.ydp.eu/ydp/projects/empiria/libs/gwtCreateJs/trunk@117876 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- src/eu/ydp/gwtcreatejs/client/loader/CreateJsContent.java
+++ src/eu/ydp/gwtcreatejs/client/loader/CreateJsContent.java
@@ -2,6 +2,7 @@ package eu.ydp.gwtcreatejs.client.loader;
 
 import com.google.gwt.canvas.client.Canvas;
 import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.user.client.Element;
 
 import eu.ydp.gwtcreatejs.client.SoundJs;
 import eu.ydp.gwtcreatejs.client.Stage;
@@ -9,6 +10,14 @@ import eu.ydp.gwtcreatejs.client.Ticker;
 
 public class CreateJsContent {
 	
+	private static final String WEBKIT_TAP_HIGHLIGHT_COLOR = "-webkit-tap-highlight-color";
+
+	private static final String OUTLINE = "outline";
+
+	private static final String HEIGHT = "height";
+
+	private static final String WIDTH = "width";
+
 	private final Canvas canvas;
 	
 	private JavaScriptObject resource;
@@ -24,8 +33,12 @@ public class CreateJsContent {
 		manifest = mnf;
 		
 		if(canvas != null){
-			canvas.getElement().setAttribute("width", String.valueOf(manifest.getWidth()));
-			canvas.getElement().setAttribute("height", String.valueOf(manifest.getHeight()));
+			Element canvasElement = canvas.getElement();
+			
+			canvasElement.setAttribute(WIDTH, String.valueOf(manifest.getWidth()));
+			canvasElement.setAttribute(HEIGHT, String.valueOf(manifest.getHeight()));
+			canvasElement.getStyle().setProperty(OUTLINE, "none");
+			canvasElement.getStyle().setProperty(WEBKIT_TAP_HIGHLIGHT_COLOR, "rgba(255, 255, 255, 0)");
 			
 			initializeResource();
 		}
